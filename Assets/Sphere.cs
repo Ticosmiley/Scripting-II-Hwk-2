ce1d219db23790feb955c591eab1d0b9870887fd
Henry Hazlett <ticotoad@gmail.com>
2020-10-11 15:21:37 -0500

    Initial project setup

---
+++ Assets/Sphere.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+    public float baseSize = 1f;
+
+    private void Update()
+    {
+        float animation = baseSize + Mathf.Sin(Time.time * 8f) * baseSize / 7f;
+        transform.localScale = Vector3.one * animation;
+    }
+}
